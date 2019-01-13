d064bd2cdfc67bac9d82c50f1bf69ca8e6d165e8
Luciano M <lm11887@gmail.com>
2019-01-13 01:11:04 -0800

    will test tomorrow

--- EternalGuardian.py
+++ EternalGuardian.py
@@ -12,11 +12,17 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
-@client.event
+@client.event # captcha
 async def on_message(message):
     if message.channel == 533125618309529620:
+        await message.channel.send('You at least got the channel right.')
         if message == 'a wizard lizard with a gizzard in a blizzard':
+            await message.channel.send('captcha successful')
             await message.author.add_roles(discord.utils.get(message.guild.roles,  name = "Adventurer"))
+            await message.delete()
+    if 'rougelike' in message:
+        message.channel.send('It\s spelled *rogue*like.')
+    await client.process_commands(message)
 
 @client.command()
 async def echo(ctx, *, text : str):
@@ -27,13 +33,16 @@ async def echo(ctx, *, text : str):
 async def _kick(ctx, member : discord.Member):
     await member.kick()
     await ctx.send('Kicked')
+
+@client.command()
+@commands.has_permissions(manage_roles=True, ban_members=True)
+async def ban(member):
+    await member.ban()
     
-@_kick.error
-async def kick_error(error, ctx):
+@client.event
+async def on_error(error, ctx):
     if isinstance(error, commands.MissingPermissions):
-        await ctx.send('Sorry, you\'re not good enough to have a special ability.')
-#@client.command()
-#async def ban(member):
-#    await client.ban(member,1)
+        await ctx.send('You sense the mods are displeased...')
+
 
 client.run(TOKEN)
