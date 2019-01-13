7a4a1c6d8bc5ac628b3a4d37e804632f32e0a7d5
Luciano M <lm11887@gmail.com>
2019-01-13 00:48:39 -0800

    First commit

---
+++ EternalGuardian.py
@@ -0,0 +1,39 @@
+import discord
+import random
+from discord.ext import commands
+from EGKeys import TOKEN
+
+client = commands.Bot(command_prefix='&')
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.channel == 533125618309529620:
+        if message == 'a wizard lizard with a gizzard in a blizzard':
+            await message.author.add_roles(discord.utils.get(message.guild.roles,  name = "Adventurer"))
+
+@client.command()
+async def echo(ctx, *, text : str):
+    await ctx.send(text)
+
+@client.command(name = "kick")
+@commands.has_permissions(manage_roles=True, ban_members=True)
+async def _kick(ctx, member : discord.Member):
+    await member.kick()
+    await ctx.send('Kicked')
+    
+@_kick.error
+async def kick_error(error, ctx):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send('Sorry, you\'re not good enough to have a special ability.')
+#@client.command()
+#async def ban(member):
+#    await client.ban(member,1)
+
+client.run(TOKEN)
---
+++ README.md
@@ -0,0 +1,5 @@
+# Eternal Guardian
+
+A moderation bot for the [Roguelikes Discord server](https://discord.gg/9pmFGKx).
+
+To run your own instance of Eternal Guardian, you will need to create "EGKeys.py" with a variable `TOKEN` set to your own bot token. 
