8fff0b34cd80ff72186f973dfa5ed0e616341487
Luciano M <lm11887@gmail.com>
2019-01-14 21:43:16 -0800

    idek what i added but there's lots lol

--- EternalGuardian.py
+++ EternalGuardian.py
@@ -1,42 +1,49 @@
 import discord
+from discord.ext import commands
+
 import random
 import asyncio
 import datetime
-from discord.ext import commands
 from EGKeys import TOKEN
 
-welcome_channel = discord.utils.get(discord.guild.channels, name='the-door')
 client = commands.Bot(command_prefix='&')
-captcha = 'a wizard lizard with a gizzard in a blizzard'
 
 @client.event
 async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
-    print('------')
+    print('------') 
+
+    global EGguild, EGwelcome_channel, EGverified_role, EGcaptcha
+    EGguild = discord.utils.get(client.guilds, id = 205277826788622337)
+    EGwelcome_channel = discord.utils.get(EGguild.text_channels, id = 533125618309529620)
+    EGverified_role = discord.utils.get(EGguild.roles, name = 'Adventurer')
+    EGcaptcha = 'a wizard lizard with a gizzard in a blizzard'
 
 async def captcha_instructions():
     await client.wait_until_ready()
-    if datetime.datetime.now().hour == 0:
-        welcome_channel.send('Welcome, Adventurer! To verify you\'re not a bot, please type the captcha: \"{}\"'.format(captcha))
+    if datetime.datetime.now().hour == 0 and datetime.datetime.now().minute == 0:
+        EGwelcome_channel.send('Welcome, Adventurer! To verify you\'re not a bot, please type the captcha: \"{}\"'.format(EGcaptcha))
 
-
-@client.event # captcha
+@client.event
 async def on_message(message):
-    if message.channel == welcome_channel:
-        await message.channel.send('You at least got the channel right.', delete_after=3.0)
-        if message.content == captcha:
-            await message.channel.send('captcha successful', delete_after=3.0)
-            verified_role = discord.utils.get(message.guild.roles,  name = "Adventurer")
-            print(verified_role)
-            await message.author.add_roles(verified_role)
+    banned_list = ['nigg', 'chink', 'noxico']
+    for word in banned_list:
+        if word in message.content:
+            mod_channel = discord.utils.get(EGguild.text_channels, id = 207537092467621889)
+            await mod_channel.send('Slur detected in {0} by user {1}.'.format(message.channel,message.author.mention))
+    if not message.author == client.user:
+        if message.channel == EGwelcome_channel:
+            if message.content == EGcaptcha:
+                await message.channel.send('captcha successful', delete_after=3.0)
+                await message.author.add_roles(EGverified_role)
+            else:
+                await message.channel.send('captcha failed',delete_after=3.0)
             await message.delete()
-    if 'rougelike' in message.content:
-        await message.channel.send('It\'s spelled *rogue*like.')
-    if 'nigg' in message.content:
-        await message.author.ban()
-        await 
+
+        if 'rougelike' in message.content:
+            await message.channel.send('It\'s spelled *rogue*like.')
     await client.process_commands(message)
 
 @client.command()
@@ -51,14 +58,27 @@ async def _kick(ctx, member : discord.Member):
 
 @client.command()
 @commands.has_permissions(manage_roles=True, ban_members=True)
-async def ban(ctx, member : discord.Member):
+async def _ban(ctx, member : discord.Member):
     await ctx.send('{0} just got SokoBanned!'.format(ctx.author.mention))
     await member.ban()
 
-@client.event
-async def on_error(error, ctx):
-    if isinstance(error, commands.MissingPermissions):
-        await ctx.send('You\'re not high enough level to do that...')
+@client.command(name = "ban")
+@commands.has_permissions(administrator=True)
+async def verifyall(ctx):
+    for vmember in ctx.guild.members():
+        await vmember.add_role(EGverified_role)
+    await ctx.send('all members verfied')
 
+@client.command()
+async def source(ctx):
+    ctx.send('https://github.com/PyrooL/EternalGuardian')
+
+@client.event
+async def on_error(ctx,error):
+    if isinstance(error, commands.CommandNotFound):  # fails silently
+        pass
+    elif isinstance(error, commands.MissingPermissions):
+        print("You\'re not high enough level to do that...")
+        await ctx.send('You\'re not high enough level to do that...')
 
 client.run(TOKEN)
